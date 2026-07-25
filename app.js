6083046264ecf12f89a21c72c4426c854322a105
c4str000 Lenovo <castrougartemauricio@gmail.com>
2026-07-24 21:11:41 -0600

    Resolviendo conflicto

--- app.js
+++ app.js
@@ -3,8 +3,4 @@ let intentos = 0;
 let listaNumerosSorteados = [];
 let numeroMaximo = 50;
 
-<<<<<<< HEAD
 teste ANDO localmente
-=======
-teste ANDO remotamente
->>>>>>> b64c68ade2006797a2c850d2dd588aa0f1e4e5d9
